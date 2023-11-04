5eda6e272f302544973392c11afaf49586759e23
Amin-farina <amiin74@gmail.com>
2023-11-04 14:26:39 -0300

    Cambio del titulo

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="container mx-auto">
         <div className="flex">
           <div className="flex flex-col justify-center w-1/2 min-h-[80vh] space-y-4">
-            Sebastián Amin, Fariña
+            <h1 class="font-bold text-[3rem]">Sebastián Amin, Fariña</h1>
             <div className="italic">
               <p>
                 I am Full Stack Developer, I have experience using modern
