d8d022695e00067a7b86c2d9c426575e4bf15957
Amin-farina <amiin74@gmail.com>
2023-11-04 13:15:59 -0300

    New pages

--- about/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function AboutPage() {
-    return (
-        <div className="justify-center items-center flex h-screen">
-            <h1>About Seccion</h1>
-        </div>
-    )
-}
