5d4f01c4c1646a0493ee30cf3868ee53bf299280
Sebastian Amin Fariña <amiin74@gmail.com>
2023-12-27 17:25:39 -0300

    Fix responsive

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
     return (
         <div className='w-100 '>
             <div className="mx-auto container">
-                <div className="flex w-100 min-h-[80vh] max-sm:flex-wrap">
+                <div className="flex w-100 min-h-[80vh] max-sm:flex-wrap justify-center items-center">
                     <div className="flex-col w-1/2 max-sm:w-full">
                         <div className='justify-center flex items-center min-sm:h-[80vh] max-sm:mb-5 max-sm:px-5'>
                             <TextContact />
